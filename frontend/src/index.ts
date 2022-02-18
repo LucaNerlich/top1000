da7f16ae1dc93ee748c03047dabfb9f29cc11274
jeanpaulrichter <webkonten.ls@hsslj.de>
2022-02-18 21:30:03 +0100

    nicer poll

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -22,6 +22,8 @@ declare module Select2 {
     }
 }
 
+let current_game: HTMLElement | undefined = undefined;
+
 function getLabel(item: ResultItem) {
     if(item.platforms !== undefined) {
         let temp;
@@ -44,7 +46,7 @@ function getLabel(item: ResultItem) {
 }
 
 window.addEventListener("load", () => {
-    $("#main select").select2({
+    $("#games select").select2({
         "ajax": {
             "url": "/search",
             "dataType": "json",
@@ -68,23 +70,86 @@ window.addEventListener("load", () => {
         }
     });
 
-    const buttons = document.querySelectorAll("#main button");
+    const buttons = document.querySelectorAll("#games button");
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", onClickButton);
     }
 });
+
+window.addEventListener("mouseup", (e) => {
+    const el = e.target as HTMLElement;
+    if(el !== null && el.tagName !== "BUTTON" && el.tagName !== "TEXTAREA") {
+        if(el.tagName === "SPAN" && el.classList.contains("select2-selection")) {
+            return;
+        } else {
+            if(current_game !== undefined) {
+                setGameSelected(current_game, false);
+            }
+        }
+    }
+});
  
 function onClickButton(this: HTMLButtonElement) {
-    const el_parent = this.parentElement as HTMLElement;
-    const el_papa = el_parent.parentElement as HTMLElement;
-    const el_div = el_papa.nextElementSibling as HTMLElement;
-    if(el_div.classList.contains("hidden")) {
-        el_div.classList.remove("hidden");
-        this.classList.remove("btn_expand_more");
-        this.classList.add("btn_expand_less");
-    } else {
+    const el_game = findGame(this);
+    setGameSelected(el_game, !el_game.classList.contains("game-selected"), true);
+}
+
+$(document).on("select2:opening", e => {
+    e.stopPropagation();
+    setGameSelected(findGame(e.target as HTMLElement), true);
+});
+
+$(document).on("select2:close", e => {
+    setGameSelected(findGame(e.target as HTMLElement), false);
+});
+
+$(document).on('select2:open', () => {
+    setTimeout(() => {
+        const x = document.querySelector('.select2-search__field') as HTMLElement;
+        if(x !== null) {
+            x.focus();
+        }
+    }, 10);    
+});
+
+function findGame(e: HTMLElement) {
+    while(e.className !== "game" && e.parentElement !== null && e.dataset.index === undefined) {
+        e = e.parentElement;
+    }
+    return e;
+}
+
+function setGameSelected(el: HTMLElement, value: boolean, comment?: boolean) {
+    const el_div = el.children[1] as HTMLElement;
+    const el_btn = el.querySelector("button") as HTMLButtonElement;
+    if(value === false) {
+        el.classList.remove("game-selected");
         el_div.classList.add("hidden");
-        this.classList.add("btn_expand_more");
-        this.classList.remove("btn_expand_less");
+        el_btn.classList.add("btn_expand_more");
+        el_btn.classList.remove("btn_expand_less");
+        if(el === current_game) {
+            current_game = undefined;
+        }
+    } else {
+        if(comment === true) {
+            el_div.classList.remove("hidden");
+            el_btn.classList.remove("btn_expand_more");
+            el_btn.classList.add("btn_expand_less");
+            const el_textarea = el_div.querySelector("textarea") as HTMLElement;
+            el_textarea.focus();
+        }
+        if(el === current_game) {
+            return;
+        }
+        el.classList.add("game-selected");
+        if(current_game !== undefined) {
+            current_game.classList.remove("game-selected");
+            const el_cur_div = current_game.children[1] as HTMLElement;
+            const el_cur_btn = current_game.querySelector("button") as HTMLButtonElement;
+            el_cur_div.classList.add("hidden");
+            el_cur_btn.classList.add("btn_expand_more");
+            el_cur_btn.classList.remove("btn_expand_less");
+        }
+        current_game = el;
     }
 }
